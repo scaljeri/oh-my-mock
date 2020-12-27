66146062a703a89747ccb7ee03ff80e12b632be2
Lucas Calje <lucas@calje.eu>
2020-12-27 15:06:54 +0100

    fix: init

---
+++ src/background.ts
@@ -0,0 +1,9 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.webNavigation.onCompleted.addListener(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+      chrome.pageAction.show(id);
+    });
+  }, { url: [{ urlMatches: 'google.com' }] });
+});
+
+sdf
