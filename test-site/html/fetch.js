3fc4e97fc94902b53e55b34f4fcc9a4f71206968
Lucas Calje <lucas@calje.eu>
2021-03-05 23:04:12 +0100

    fix: testing with fetch

---
+++ test-site/html/fetch.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  const btn = document.querySelector('button.fetch');
+  const parent = document.querySelector('section.fetch');
+  btn.addEventListener('click', () => {
+    fetch('/users').then(response => {
+      const header = [...response.headers.entries()];
+      const headerEl = parent.querySelector('.headers');
+
+      headerEl.innerHTML = ':</br>' + header.reduce((out, item) => out + `${item[0]}: ${item[1]}<br>`, '');
+      // const code = document.querySelector('.get-code');
+      // code.classList.remove('hidden');
+      // const data = JSON.parse(xhr.responseText);
+      // code.innerText = JSON.stringify(data, null, 4)
+      // document.querySelector('.status-code').innerText = xhr.status;
+      // document.querySelector('.headers').innerText = xhr.getAllResponseHeaders();
+    });
+  });
+});
--- test-site/html/get.js
+++ test-site/html/get.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded', function(event) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', '/users');
     xhr.addEventListener('load', (res) => {
-      const code = document.querySelector('.get-code');
+      const code = document.querySelector('.get .get-code');
       code.classList.remove('hidden');
       const data = JSON.parse(xhr.responseText);
       code.innerText = JSON.stringify(data, null, 4)
-      document.querySelector('.status-code').innerText = xhr.status;
-      document.querySelector('.headers').innerText = xhr.getAllResponseHeaders();
+      document.querySelector('.get .status-code').innerText = xhr.status;
+      document.querySelector('.get .headers').innerText = xhr.getAllResponseHeaders();
       debugger;
     });
     xhr.send();
