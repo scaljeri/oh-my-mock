153ef9b886df200db3bfa23f651d7eb0d51e0141
Lucas Calje <lucas@calje.eu>
2021-03-24 18:02:04 +0100

    fix: ci

--- src/app/components/file-uploader/file-uploader.component.spec.ts
+++ src/app/components/file-uploader/file-uploader.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FileUploaderComponent } from './file-uploader.component';
@@ -8,7 +9,8 @@ describe('FileUploaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FileUploaderComponent ]
+      declarations: [ FileUploaderComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
