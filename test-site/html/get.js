2e9e4c71abbb9fa5544cbcff2260bb109586b4b9
Lucas Calje <lucas@calje.eu>
2021-04-03 11:32:21 +0200

    fix: cleanup

--- test-site/html/get.js
+++ test-site/html/get.js
@@ -13,21 +13,9 @@ window.ohMyMock.xhr = (method, responseType, cb) => {
     xhr.open(method, RESPONSE_MAP[responseType]);
     xhr.onreadystatechange = cb;
     xhr.addEventListener("load", (res) => {
-
       window.ohMyMock.responseFn(xhr.responseText);
       window.ohMyMock.statusCodeFn(xhr.status);
-      console.log(xhr.getAllResponseHeaders());
       window.ohMyMock.headersFn(xhr.getAllResponseHeaders());
-
-      // const code = document.querySelector(".get .get-code");
-      // code.classList.remove("hidden");
-      // const data = JSON.parse(xhr.responseText);
-      // code.innerText = JSON.stringify(data, null, 4);
-      // document.querySelector(".get .status-code").innerText = xhr.status;
-      // document.querySelector(
-      //   ".get .headers"
-      // ).innerText = xhr.getAllResponseHeaders();
-      // document.querySelector('.duration').innerHTML = `&nbsp;${Math.round((Date.now()  - start)/1000)} seconds`;
     });
     xhr.send();
 };
