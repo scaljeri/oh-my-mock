05cb1722fdbdb1cef08d6a57c03074e0645fe965
Lucas Calje <lucas@calje.eu>
2021-04-06 19:59:28 +0200

    fix: mock-js move return to new line

--- src/shared/utils/eval-jscode.ts
+++ src/shared/utils/eval-jscode.ts
@@ -1,5 +1,5 @@
 export const evalJsCode = (code: string): ((mock) => unknown) => {
-  code = `let output;${code};return output;`;
+  code = `let output;${code};\n\nreturn output;`;
   // tslint-disable-next
   const fnc = eval(new Function('mock', code) as any);
   return fnc;
