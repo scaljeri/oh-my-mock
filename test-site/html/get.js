1699b061d713402a7932abb41b7e76b0739f9792
Lucas Calje <lucas@calje.eu>
2021-04-06 19:48:28 +0200

    fix: fetch request

--- test-site/html/get.js
+++ test-site/html/get.js
@@ -1,16 +1,7 @@
-if (!window.ohMyMock) {
-  window.ohMyMock = {};
-}
-
-const RESPONSE_MAP = {
-  html: '/site',
-  json: '/users'
-}
-
 window.ohMyMock.xhr = (method, responseType, cb) => {
 
     const xhr = new XMLHttpRequest();
-    xhr.open(method, RESPONSE_MAP[responseType]);
+    xhr.open(method, ohMyMock.urlMap[responseType]);
     xhr.onreadystatechange = cb;
     xhr.addEventListener("load", (res) => {
       window.ohMyMock.responseFn(xhr.responseText);
@@ -18,4 +9,5 @@ window.ohMyMock.xhr = (method, responseType, cb) => {
       window.ohMyMock.headersFn(xhr.getAllResponseHeaders());
     });
     xhr.send();
+
 };
