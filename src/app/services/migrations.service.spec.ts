a714adec70970e66f432b6a9d1216a76e54ae648
Lucas Calje <lucas@calje.eu>
2021-05-03 22:32:01 +0200

    fix: tests

--- src/app/services/migrations.service.spec.ts
+++ src/app/services/migrations.service.spec.ts
@@ -1,16 +1,62 @@
-import { TestBed } from '@angular/core/testing';
-
 import { MigrationsService } from './migrations.service';
+
+const CURRENT_VERSION = '__OH_MY_VERSION__';
 
 describe('MigrationsService', () => {
   let service: MigrationsService;
+  let state: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MigrationsService);
+    state = {
+      version: CURRENT_VERSION,
+      data: 'data'
+    };
+    service = new MigrationsService({ version: CURRENT_VERSION } as any)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('Up2Date', () => {
+    beforeEach(() => {
+    });
+
+    it('should not modify the data if it has current version', () => {
+      const out = service.update(state);
+      expect(out).toEqual(state);
+    });
+  });
+
+  describe('Older version', () => {
+    it('should reset the data', () => {
+      state.version = '2.0.0';
+      const out = service.update(state);
+      expect(out).toEqual(service.reset());
+    });
+
+    it('should migrate the data', () => {
+      state.version = '2.1.0';
+      const out = service.update(state);
+      expect(out).toEqual({ ...state, version: '__OH_MY_VERSION__' });
+    })
+  });
+
+  describe('Unknown version', () => {
+    it('should reset the data', () => {
+      state.version = '1.9.0';
+      const out = service.update(state);
+      expect(out).toEqual(service.reset());
+    });
+  });
+
+  describe('No version', () => {
+    it('should reset the data', () => {
+      delete state.version;
+      const out = service.update(state);
+      expect(out).toEqual(service.reset());
+    });
+  });
+
+
 });
--- src/app/services/migrations.service.ts
+++ src/app/services/migrations.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { IOhMyMock } from '@shared/type';
 import { AppStateService } from './app-state.service';
 
-const MIGRATION_MAP = {
+export const MIGRATION_MAP = {
   // This is a token that is replaced in the application by the token-repace.js script. But,
   // during development is not replace (and it should not be replaced here) so we need to take care of it
-  ['__OH_MY_' + 'VERSION__']: { next: '2.0.0', migrate: (_) => _ },
-  '0.0.0': { next: '2.0.0', migrate: () => null },
-  '2.0.0': {}
+  ['__OH_MY_' + 'VERSION__']: {}, // for development only
+  '0.0.0': { next: '2.0.0', migrate: (_) => null },
+  '2.0.0': { next: '2.1.0', migrate: (_) => null },
+  '2.1.0': { next: '__OH_MY_VERSION__', migrate: (_) => _ },
+  '__OH_MY_VERSION__': {}
 }
 
 @Injectable({
@@ -21,7 +23,7 @@ export class MigrationsService {
     state = { ...state };
     const version = state.version;
 
-    if (version && (!MIGRATION_MAP[version] || (version > this.appState.version && !this.appState.version.match(/^__/)))) {
+    if (version && (!MIGRATION_MAP[version] || (version > this.appState.version && this.appState.version.match(/^__/)))) {
       // This can only happen with imports. The App version is lower than the imported data
       return this.reset();
     }
