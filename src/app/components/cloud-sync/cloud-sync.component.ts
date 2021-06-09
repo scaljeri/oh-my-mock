6e0fec42fd128482d5a23b288c20896157f9b994
Lucas Calje <lucas@calje.eu>
2021-06-09 22:44:50 +0200

    fix: cleanup

--- src/app/components/cloud-sync/cloud-sync.component.ts
+++ src/app/components/cloud-sync/cloud-sync.component.ts
@@ -1,20 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { CloudSyncService } from '../../services/cloud-sync.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'oh-my-cloud-sync-svg',
   templateUrl: './cloud-sync.component.svg',
   styleUrls: ['./cloud-sync.component.scss']
 })
-export class CloudSyncComponent implements OnInit {
-  isSyncing = false;
-
-  constructor(public cloudSyncService: CloudSyncService) { }
-
-  ngOnInit(): void {
-    this.cloudSyncService.activity$.subscribe(v => {
-      console.log(v);
-    })
-  }
-
+export class CloudSyncComponent {
 }
