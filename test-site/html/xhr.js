9640392e2e697bf1331f67499211e0c1b960b7aa
Lucas Calje <lucas@calje.eu>
2021-06-19 23:31:52 +0200

    fix: test onload xhr

--- test-site/html/xhr.js
+++ test-site/html/xhr.js
@@ -5,11 +5,26 @@ window.ohMyMock.xhr = (method, responseType, cb) => {
     xhr.setRequestHeader('xxxxxxxxx', 'yyyyyyyyyy');
 
     xhr.onreadystatechange = cb;
+    let count = 0;
     xhr.addEventListener("load", (res) => {
-      window.ohMyMock.responseFn(xhr.responseText);
-      window.ohMyMock.statusCodeFn(xhr.status);
-      window.ohMyMock.headersFn(xhr.getAllResponseHeaders());
+      count ++;
+      if (count === 2) {
+        xhrInjectResponse(xhr);
+      }
     });
+
+    xhr.onload = () => {
+      count ++;
+      if (count === 2) {
+        xhrInjectResponse(xhr);
+      }
+    }
     xhr.send(method === "POST" ? { x: 10 } : null);
 
 };
+
+function xhrInjectResponse(xhr) {
+  window.ohMyMock.responseFn(xhr.responseText);
+  window.ohMyMock.statusCodeFn(xhr.status);
+  window.ohMyMock.headersFn(xhr.getAllResponseHeaders());
+}
