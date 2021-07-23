2b2e922aa7e8147c486a68923c59655d23ba0659
Lucas Calje <lucas@calje.eu>
2021-07-23 10:52:46 +0200

    fix: import json into active domain (#94)
    * fix: import json into active domain
    
    * fix(sdk): use context props to override mock defaults
    
    * chore(sdk): new version
    
    * fix: make dialog optional and fix test

--- src/app/components/json-import/json-import.component.spec.ts
+++ src/app/components/json-import/json-import.component.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngxs/store';
 
 import { JsonImportComponent } from './json-import.component';
@@ -12,7 +13,7 @@ describe('JsonImportComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ JsonImportComponent ],
       schemas: [NO_ERRORS_SCHEMA],
-      providers: [{provide: Store, useValue: {}}]
+      providers: [{ provide: Store, useValue: {} }, { provide: MatDialogRef, useValue: {close: jest.fn()}}]
     })
     .compileComponents();
   });
