eee9b967048f791045cc7821abf3a7b1bc9cf54e
Lucas Calje <lucas@calje.eu>
2021-10-26 22:56:36 +0200

    fix: update version

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,2 @@
 // export const presetInfo = 'Enable a whole group of mocks by selecting a preset. To edit or delete presets click the edit icon';
-export const presetInfo = 'Select one response for each request per preset';
+export const presetInfo = 'Responses can be enabled or disabled per preset';
