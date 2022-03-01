05066b8a8a9d2bfbd22f3e9bbb37a6053e86e1f8
Lucas Calje <lucas@calje.eu>
2022-03-01 21:31:46 +0100

    fix: xhr complete

--- src/injected/state-manager.ts
+++ src/injected/state-manager.ts
@@ -6,33 +6,31 @@ import { OhMyMessageBus } from '../shared/utils/message-bus';
 import { triggerWindow } from '../shared/utils/trigger-msg-window';
 
 let state: IState;
-const update = new BehaviorSubject<IState>(state);
-const mb = new OhMyMessageBus().setTrigger(triggerWindow);
 
-mb.streamByType$(payloadType.STATE, appSources.CONTENT).subscribe(({ packet }) => {
-  state = packet.payload.data as IState;
-  update.next(state);
-});
+export function setupListenersMessageBus() {
+  const update = new BehaviorSubject<IState>(state);
+  const mb = new OhMyMessageBus().setTrigger(triggerWindow);
+  window[STORAGE_KEY].off.push(() => mb.clear());
 
-mb.streamByType$(payloadType.ACTIVE, appSources.CONTENT).subscribe(({ packet }) => {
-  // state = packet.payload.data as IState;
-  // update.next(state);
-  // INJECTED SCRIPT: state-manger.ts
-});
+  mb.streamByType$(payloadType.STATE, appSources.CONTENT).subscribe(({ packet }) => {
+    state = packet.payload.data as IState;
+    update.next(state);
+  });
 
-console.log('SETUP LISTENER INJECTED FOR PRE');
-mb.streamByType$<IOhMyReadyResponse>(payloadType.RESPONSE, appSources.CONTENT).subscribe(({ packet }: IOhMessage<IOhMyReadyResponse>) => {
-  // state = packet.payload.data as IState;
-  // update.next(state);
-  // INJECTED SCRIPT: state-manger.ts
-  console.log('----------------------------------------', packet)
-  if (packet.payload.data.response.status === ohMyMockStatus.OK) {
-    console.log('PRE_RESPONSE', packet.payload);
-    window[STORAGE_KEY].cache.push(packet.payload.data);
-  }
-});
+  mb.streamByType$(payloadType.ACTIVE, appSources.CONTENT).subscribe(({ packet }) => {
+    // state = packet.payload.data as IState;
+    // update.next(state);
+    // INJECTED SCRIPT: state-manger.ts
+  });
 
-export const ohMyState$ = update.asObservable();
+  mb.streamByType$<IOhMyReadyResponse>(payloadType.RESPONSE, appSources.CONTENT).subscribe(({ packet }: IOhMessage<IOhMyReadyResponse>) => {
+    if (packet.payload.data.response.status === ohMyMockStatus.OK) {
+      window[STORAGE_KEY].cache.push(packet.payload.data);
+    }
+  });
+
+  return update.asObservable();
+}
 
 export const ohMyState = (): IState => {
   return state;
