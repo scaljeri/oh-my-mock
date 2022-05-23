317f8d9506edb3974517631bc6d3d20c3537af98
Lucas Calje <lucas@calje.eu>
2022-05-23 22:22:13 +0200

    fix: missing status code fetch

--- src/background/handlers/response-handler.ts
+++ src/background/handlers/response-handler.ts
@@ -1,5 +1,5 @@
 import { IOhMyResponseUpdate, IOhMyPacketContext, IPacketPayload } from "../../shared/packet-type";
-import { IMock, IState } from "../../shared/type";
+import { IData, IMock, IState } from "../../shared/type";
 import { MockUtils } from "../../shared/utils/mock";
 import { update } from "../../shared/utils/partial-updater";
 import { OhMyQueue } from "../../shared/utils/queue";
@@ -8,6 +8,7 @@ import { StorageUtils } from "../../shared/utils/storage";
 import { DataUtils } from "../../shared/utils/data";
 import { payloadType } from "../../shared/constants";
 import { timestamp } from "../../shared/utils/timestamp";
+import { deepSearch, shallowSearch, splitIntoSearchTerms } from '../../shared/utils/search';
 // import { shallowSearch, splitIntoSearchTerms } from "../../shared/utils/search";
 
 export class OhMyResponseHandler {
@@ -57,6 +58,8 @@ export class OhMyResponseHandler {
         // }
       }
 
+      OhMyResponseHandler.updateFiltering(state, request, response as IMock);
+
       OhMyResponseHandler.queue.addPacket(payloadType.STATE, {
         payload: {
           data: request,
@@ -70,7 +73,64 @@ export class OhMyResponseHandler {
 
       return StorageUtils.set(response.id, response).then(() => response as IMock);
     } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log('nonoo', err);
+    }
+  }
 
+  static async updateFiltering(state: IState, request: IData, response: IMock) {
+    try {
+      const searchTerms = splitIntoSearchTerms(state.aux.filterKeywords);
+
+      if (!searchTerms.length) {
+        return;
+      }
+
+      const searchOpts = state.aux.filterOptions;
+      const data = { [request.id]: request };
+
+      const results = shallowSearch(data, searchTerms, searchOpts);
+      let addRequest = !!results[request.id];
+      if (!addRequest) { // Deep search needed
+        const mocks = (await Promise.all(Object.keys(request.mocks).filter(mid => mid !== response.id).map(
+          mid => StorageUtils.get<IMock>(mid)))
+        ).reduce((acc, mock) => {
+          acc[mock.id] = mock;
+          return acc;
+        }, { [response.id]: response });
+
+        const matches = await deepSearch(data, searchTerms, searchOpts, mocks);
+        addRequest = matches.length > 0;
+      }
+
+      const index = (state.aux.filteredRequests || []).indexOf(request.id);
+
+      let isUpdated = false;
+      if (addRequest && index === -1) {
+        isUpdated = true;
+        state.aux.filteredRequests = state.aux.filteredRequests ? [...state.aux.filteredRequests, request.id] : [request.id];
+        return state;
+      } else if (!addRequest && index > -1) {
+        isUpdated = true;
+        state.aux.filteredRequests.splice(index, 1);
+        return state;
+      }
+
+      if (isUpdated) {
+        OhMyResponseHandler.queue.addPacket(payloadType.STATE, {
+          payload: {
+            data: state.aux.filteredRequests,
+            context: {
+              path: `$.aux`,
+              propertyName: 'filteredRequests',
+              domain: state.domain
+            } as IOhMyPacketContext
+          }
+        });
+      }
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error('Could not update filter results', err);
     }
   }
 }
