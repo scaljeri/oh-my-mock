fb235743f99ac249711f9107af433ec14b9926f7
Lucas Calje <lucas@calje.eu>
2022-05-30 20:26:43 +0200

    fix: headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,7 +45,7 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
   },
